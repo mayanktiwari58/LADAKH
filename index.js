e85ae23e5fcb45ec815a03084997c8cadceb74d5
mayanktiwari58 <76210281+mayanktiwari58@users.noreply.github.com>
2021-07-10 03:11:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+function changebackground(){
+    document.getElementById("button-send").style.backgroundColor= 'green';
+}
+
+function heading1() { 
+    document.getElementById("ladakh1").style.color = 'black';
+  }
+
+   function btn1(){
+    document.getElementById("btn1").style.backgroundColor= 'yellow';
+   }
+   
+   function btn2(){
+    document.getElementById("btn2").style.backgroundColor= 'blue';
+   }
+   
+   function li() { 
+     document.getElementById("li").style.color = 'brown';
+   }
+   function li2() { 
+     document.getElementById("li2").style.color = 'yellow';
+   }
+   function li3() { 
+     document.getElementById("li3").style.color = 'green';
+   }
+   function li4() { 
+     document.getElementById("li4").style.color = 'blue';
+   }
+   function li5() { 
+     document.getElementById("li5").style.color = 'red';
+   }
+   function changeColor() { 
+     document.getElementById("li6").style.color = 'pink';
+   }
